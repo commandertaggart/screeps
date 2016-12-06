9c8bca4821423bb27ebef2e5db1a27a4486947f7
Ben Taggart <benjamin.taggart@disney.com>
2016-12-05 18:48:45 -0800

    small fixes

--- dev/main.js
+++ dev/main.js
@@ -1,13 +1,23 @@
 
-global.manager = {};
-[ 'flag', 'population', 'task' ]
-    .forEach(x => manager[x] = require('./manager.'+x));
-global.role = {};
-[ 'queue', 'worker' ]
-    .forEach(x => manager[x] = require('./role.'+x));
-global.task = {};
-[ 'build', 'gather', 'queue', 'repair', 'store', 'upgrade' ]
-    .forEach(x => manager[x] = require('./task.'+x));
+global.manager = {
+    flag: require('./manager.flag'),
+    population: require('./manager.population'),
+    task: require('./manager.task')
+};
+
+global.role = {
+    worker: require('./role.worker'),
+    queue: require('./role.queue')
+};
+
+global.task = {
+    build: require('./task.build'),
+    gather: require('./task.gather'),
+    queue: require('./task.queue'),
+    repair: require('./task.repair'),
+    store: require('./task.store'),
+    upgrade: require('./task.upgrade'),
+};
 
 module.exports.loop = function main_loop()
 {
